1276d747385cee9693e16181cb57cf242ad8f8fd
Caleb Hankins <chanki@acxiom.com>
2019-11-25 18:48:26 +0000

    promise playground

---
+++ promises/cleanup.js
@@ -0,0 +1,15 @@
+// Experimenting with promises, examples based on https://tech.io/playgrounds/6730/i-promise-i-wont-callback-anymore
+
+const fs = require('fs')
+
+// Get the promisify method from the util module
+const { promisify } = require('util')
+
+// Promisify unlink function
+const unlinkFile = promisify(fs.unlink)
+
+// cleanup files we created in index.js
+unlinkFile('clone.txt')
+.then(() => console.log('clone.txt unlinked!'))
+.then(() => unlinkFile('original.txt'))
+.catch((err) => console.error('Error occurred during unlink:', err))
