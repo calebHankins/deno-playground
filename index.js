66bd035c7add0a785574527cc2e75e6a9ffa1647
Caleb Hankins <chanki@acxiom.com>
2020-02-05 17:36:11 +0000

    Updating default index.js to print env info

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello, World!')
+// Simple app that prints environment info
+const os = require('os')
+const platform = os.platform()
+
+console.log(`Hello from hostname:${os.hostname()}, platform:${platform}, release:${os.release()}, arch:${os.arch()}, node runtime version:${process.version}`)
+
+if (os.release().includes('linuxkit')) {
+    console.log("Hello from container land!")
+}
+
+console.log('See ya!')
