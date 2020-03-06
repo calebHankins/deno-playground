2ec3302506e2f8a2481639291b24aff3d55076df
Caleb Hankins <chanki@acxiom.com>
2020-03-06 18:40:38 +0000

    Added eslint rules extending air-bnb and supporting linting extensions and config. Then experienced great shame

--- async-await/index.js
+++ async-await/index.js
@@ -2,46 +2,59 @@
 // https://codeburst.io/async-patterns-in-node-js-only-4-different-ways-to-do-it-70186ee83250?gi=38693e20e33c
 
 // https://blog.bitsrc.io/understanding-javascript-async-and-await-with-examples-a010b03926ea
-const fs = require('fs')
+const fs = require('fs');
+
+/**
+ * getFileContents gets file contents
+ * @param {string} fileName - file to get contents from
+ * @returns {Promise} - promise of returned contents
+ */
 function getFileContents(fileName) {
     return new Promise((resolve, reject) => {
-        fs.readFile('hello.txt', 'utf-8', (err, data) => {
-            if (err) reject(err)
-            else resolve(data)
-        })
-    })
+        fs.readFile(fileName, 'utf-8', (err, data) => {
+            if (err) reject(err);
+            else resolve(data);
+        });
+    });
 }
 
-// https://stackoverflow.com/a/41957152
+/**
+ * getFileContents gets file contents
+ *  https://stackoverflow.com/a/41957152
+ * @param {number} ms - milliseconds to sleep
+ * @returns {Promise} - promise of setTimeout
+ */
 function sleep(ms) {
-    return new Promise(resolve => {
-        setTimeout(resolve, ms)
-    })
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
 }
 
 // Call and use .then and .catch to operate on the returned promise
-getFileContents('hello.txt').then(data => {
-    console.log('File contents:', data)
-}).catch(err => {
-    console.error("Error reading file:", err)
-})
+getFileContents('hello.txt').then((data) => {
+    console.log('File contents:', data);
+}).catch((err) => {
+    console.error('Error reading file:', err);
+});
 
-// async version
+/**
+ * getFileContents gets file contents - async version
+ * @returns {void}
+ */
 async function showFileContents() {
-    console.log('Ima try an open this bad boy')
+    console.log('Ima try an open this bad boy');
     try {
-        let data = await getFileContents('hello.txt')
-        console.log('File contents (data):', data)
-        await sleep(1000)
-        let bonusData = await getFileContents('hello.txt')
-        console.log('File contents (bonusData):', bonusData)
-        console.log('This runs after both awaits have returned!')
-    }
-    catch (err) {
-        console.error('shwoopsie!', err)
+        const data = await getFileContents('hello.txt');
+        console.log('File contents (data):', data);
+        await sleep(1000);
+        const bonusData = await getFileContents('hello.txt');
+        console.log('File contents (bonusData):', bonusData);
+        console.log('This runs after both awaits have returned!');
+    } catch (err) {
+        console.error('shwoopsie!', err);
     }
 }
 
-console.log('Let\'s do this! Calling showFileContents')
-showFileContents()
-console.log('Just below showFileContents call now')
+console.log('Let\'s do this! Calling showFileContents');
+showFileContents();
+console.log('Just below showFileContents call now');
--- emoji/days-till-xmas/index.js
+++ emoji/days-till-xmas/index.js
@@ -1,45 +1,50 @@
+/* eslint-disable no-nested-ternary */ // don't do this trash irl
 // Default our start date to now, but also accept the first command line arg
-const startDate =
-    process.argv[2] ? new Date(process.argv[2])
-        : new Date()
+const startDate = process.argv[2] ? new Date(process.argv[2])
+    : new Date();
 
 // Figure out how many days we have to wait for our presents
-const daysTillXmas = getDaysTillXmas(startDate)
+const daysTillXmas = getDaysTillXmas(startDate);
 
 // Assess how we feel about that
-const mood =
-    isNaN(daysTillXmas) ? '🤔'
-        : daysTillXmas > 30 ? '🤢'
-            : daysTillXmas > 7 ? '😄'
-                : daysTillXmas > 1 ? '😁'
-                    : '🎅'
+const mood = Number.isNaN(daysTillXmas) ? '🤔'
+    : daysTillXmas > 30 ? '🤢'
+        : daysTillXmas > 7 ? '😄'
+            : daysTillXmas > 1 ? '😁'
+                : '🎅';
 
 // Then tell folks
 console.log(`It's ${daysTillXmas} 📆  till 🎄, my dudes ${mood}`);
 
 /**
  * Returns the next date of Xmas relative to the supplied StartDate
- * @param StartDate Date A starting date from which to calculate the next Xmas coming up
- *
+ * @param {Date} StartDate - A starting date from which to calculate the next Xmas coming up
+ * @returns {number} daysLeft - Days left until xmas
  */
 function getDaysTillXmas(StartDate) {
     // First guess for the date of a new xmas.
     // Months are 0 offset (obv...)
     // https://stackoverflow.com/questions/32759116/javascript-date-new-month-offset
-    const nextXmasFirstGuess = new Date(StartDate.getFullYear(), 11, 25)
+    const nextXmasFirstGuess = new Date(StartDate.getFullYear(), 11, 25);
 
     // How many days do we have left?
-    let daysLeft = getDaysBetween(StartDate, nextXmasFirstGuess)
+    let daysLeft = getDaysBetween(StartDate, nextXmasFirstGuess);
 
     // If we're past xmas for the year, roll over to next year
     if (daysLeft < 0) {
-        const nextXmasSecondGuess = new Date(StartDate.getFullYear() + 1, 11, 25)
-        daysLeft = getDaysBetween(StartDate, nextXmasSecondGuess)
+        const nextXmasSecondGuess = new Date(StartDate.getFullYear() + 1, 11, 25);
+        daysLeft = getDaysBetween(StartDate, nextXmasSecondGuess);
     }
-    return daysLeft
+    return daysLeft;
 }
 
-// https://stackoverflow.com/questions/2627473/how-to-calculate-the-number-of-days-between-two-dates
+/**
+ * Returns the number of days between the supplied start and end dates
+ * 'Inspired' by https://stackoverflow.com/questions/2627473/how-to-calculate-the-number-of-days-between-two-dates
+ * @param {Date} StartDate - starting date
+ * @param {Date} EndDate - end date
+ * @returns {number} daysBetween - number of days between start and end date
+ */
 function getDaysBetween(StartDate, EndDate) {
     // The number of milliseconds in all UTC days (no DST)
     const oneDay = 1000 * 60 * 60 * 24;
